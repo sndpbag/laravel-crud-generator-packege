da07e1bba56bf9b84975c8f1e882810ecce82047
sndp bag <sndpbagg@gmail.com>
2025-10-24 08:41:29 +0530

    make service provider file

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -1,1 +1,44 @@
-"<?php\n\nnamespace sndpbag\CrudGenerator;\n\nuse Illuminate\Support\ServiceProvider;\n\nclass CrudGeneratorServiceProvider extends ServiceProvider {\n    public function register() {}\n    public function boot() {}\n}" 
+<?php
+
+namespace Sndpbag\CrudGenerator;
+
+use Illuminate\Support\ServiceProvider;
+use Sndpbag\CrudGenerator\Commands\MakeCrudCommand;
+use Sndpbag\CrudGenerator\Commands\DeleteCrudCommand;
+
+class CrudGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/crud-generator.php', 'crud-generator'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Publish config file
+        $this->publishes([
+            __DIR__.'/../config/crud-generator.php' => config_path('crud-generator.php'),
+        ], 'crud-generator-config');
+
+        // Publish stubs
+        $this->publishes([
+            __DIR__.'/../stubs' => base_path('stubs/crud-generator'),
+        ], 'crud-generator-stubs');
+
+        // Register commands
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeCrudCommand::class,
+                DeleteCrudCommand::class,
+            ]);
+        }
+    }
+}
